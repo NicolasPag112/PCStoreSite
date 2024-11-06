c0b050701a593e37a9f67e278049eb710dc88bb5
NicolasPag112 <nicolaspag112@gmail.com>
2024-11-06 12:59:03 -0300

    Filtros de CPU e GPU adicionados

--- busca.js
+++ busca.js
@@ -11,4 +11,114 @@ $(document).ready(function(){
         }
     });
 
+    $('#minPrice').on('input', function() {
+
+        const minValue = $('#minPrice').val();
+        const maxValue = $('#maxPrice').val();
+
+        if(parseInt(maxValue) < parseInt(minValue)){
+            $('#minValue').text("Erro: valor mínimo precisa ser menor que valor máximo");
+            $('#maxValue').text("Erro: valor máximo precisa ser maior que valor mínimo");
+        } else {
+            $('#minValue').text("A partir de R$" + minValue);
+        }
+
+        
+
+    });
+    $('#maxPrice').on('input', function() {
+
+        const minValue = $('#minPrice').val();
+        const maxValue = $('#maxPrice').val();
+
+        if(parseInt(maxValue) < parseInt(minValue)){
+            $('#minValue').text("Erro: valor mínimo precisa ser menor que valor máximo");
+            $('#maxValue').text("Erro: valor máximo precisa ser maior que valor mínimo");
+        } else {
+            $('#maxValue').text("Até R$" + maxValue);
+        }
+
+        
+
+    });
+
+    $('.productType').on('change', function() {
+        if ($(this).is(':checked')) {
+            $('.productType').not(this).prop('checked', false);
+        }
+
+        if ($('#CPU').is(':checked')) {
+            $('#CPUBrands').show();
+        } else {
+            $('#CPUBrands').hide();
+
+            $('.CPUBrandType').prop('checked', false);
+            $('.CPUModel').prop('checked', false);
+
+            $('#IntelModels').hide();
+            $('#AMDModelsCPU').hide();
+        }
+
+        if ($('#GPU').is(':checked')) {
+            $('#GPUBrands').show();
+        } else {
+            $('#GPUBrands').hide();
+
+            $('.GPUBrandType').prop('checked', false);
+            $('.GPUModel').prop('checked', false);
+
+            $('#NvidiaModels').hide();
+            $('#AMDModelsGPU').hide();
+        }
+
+    });
+
+    $('.CPUBrandType').on('change', function() {
+        if ($(this).is(':checked')) {
+            $('.CPUBrandType').not(this).prop('checked', false);
+        }
+
+        if ($('#intel').is(':checked')) {
+            $('#IntelModels').show();
+        } else {
+            $('#IntelModels').hide();
+        }
+
+        if ($('#AMDCPU').is(':checked')) {
+            $('#AMDModelsCPU').show();
+        } else {
+            $('#AMDModelsCPU').hide();
+        }
+    });
+
+    $('.CPUModel').on('change', function() {
+        if ($(this).is(':checked')) {
+            $('.CPUModel').not(this).prop('checked', false);
+        }
+    })
+
+    $('.GPUBrandType').on('change', function() {
+        if ($(this).is(':checked')) {
+            $('.GPUBrandType').not(this).prop('checked', false);
+        }
+
+        if ($('#Nvidia').is(':checked')) {
+            $('#NvidiaModels').show();
+        } else {
+            $('#NvidiaModels').hide();
+        }
+
+        if ($('#AMDGPU').is(':checked')) {
+            $('#AMDModelsGPU').show();
+        } else {
+            $('#AMDModelsGPU').hide();
+        }
+    });
+
+    $('.GPUModel').on('change', function() {
+        if ($(this).is(':checked')) {
+            $('.GPUModel').not(this).prop('checked', false);
+        }
+    })
+
 });
